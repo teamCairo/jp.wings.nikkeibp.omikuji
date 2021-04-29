d0c2f13b0d14ac7b10bc0b0e538e1bcf61dbb0a4
k12hirano <k12hirano@gmail.com>
2021-04-29 14:21:03 +0900

    result,setting add

---
+++ app/src/main/java/com/example/jpwingsnikkeibpomikuji/ResultActivity.kt
@@ -0,0 +1,43 @@
+package com.example.quizapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Context
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val TextView: TextView = findViewById<TextView>(R.id.resultLabel)
+        var score = intent.getIntExtra(
+            "RIGHT_ANSWER_COUNT", 0
+        )
+        //resultLabel.setText(score.toString()+ "/5")
+
+        val resultLabel = findViewById<TextView>(R.id.resultLabel)
+        //val totalScoreLabel = findViewById<TextView>(R.id.totalScoreLabel)
+
+        //正解数を取得
+        score = intent.getIntExtra("RIGHT_ANSWER_COUNT", 0)
+
+        val prefs = getSharedPreferences("quizApp", Context.MODE_PRIVATE)
+        //var totalScore = prefs.getInt("totalScore", 0)
+
+        //トータルスコアに今回のスコアを加算
+        //totalScore += score
+
+       //テキスト表示
+        resultLabel.text = "$score /"
+        //totalScoreLabel.text = ""
+
+        //トータルスコアを保存
+        //val editor = prefs.edit()
+        //editor.putInt("totalScore", totalScore)
+        //editor.apply()
+    }
+}
+
+
+//最終スコアのデータ受け取り→表示
---
+++ app/src/main/java/com/example/jpwingsnikkeibpomikuji/SettingActivity.kt
@@ -0,0 +1,79 @@
+package com.example.quizapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.quizapplication.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+
+    private val spinnerItems = arrayOf("5", "10", "15", "20", "25", "30")
+
+    private lateinit var binding: ActivitySettingBinding
+    var selectedItem: String = ""
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Spinnerの取得
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        //Adapterの生成
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerItems)
+
+        //View binding
+        binding.spinner.adapter = arrayAdapter
+
+        //リスナーを登録
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            //アイテムが選択されたとき
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?, positionn: Int, id: Long) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                selectedItem = item
+                //View Binding
+                //binding.textView.text = item
+            }
+
+            //アイテムが選択されなかった時
+            override fun onNothingSelected(parent: AdapterView<*>?) = Unit
+        }
+
+        //選択肢の各項目のレイアウト
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        //AdapterをSpinnerのAdapterとして設定
+        spinner.adapter = arrayAdapter
+
+
+
+
+        Log.d("MyApp", "これから遷移")
+        binding.button.setOnClickListener {
+            val intent = Intent(application, MainActivity::class.java)
+            val str = selectedItem
+            //
+            //
+
+            intent.putExtra("QUIZ_QUANTITY", str)
+            startActivity(intent)
+        }
+
+
+    }
+}
+
+
+
+    //遷移ボタン内のプロパティの中身をオーバーライドから呼び出し
